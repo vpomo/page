3c24d165c000aa0c4ceb0834de66306b0aed77a5
vladimirmyshkovski <vladimirmyshkovski@gmail.com>
2021-11-26 19:05:14 +0400

    Update CryptoPageToken contract

--- contracts/CryptoPageToken.sol
+++ contracts/CryptoPageToken.sol
@@ -5,13 +5,26 @@ pragma solidity ^0.8.4;
 import "./Stakeable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract PageToken is ERC20("PageToken", "PAGE"), Stakeable, Ownable {
-    function mint(address to, uint256 amount) public onlyOwner {
+contract PageToken is
+    ERC20("PageToken", "PAGE"),
+    Stakeable,
+    AccessControl,
+    Ownable
+{
+    bytes32 private constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 private constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
-    function burn(address to, uint256 amount) public onlyOwner {
+    function burn(address to, uint256 amount) public onlyRole(BURNER_ROLE) {
         _burn(to, amount);
     }
 
