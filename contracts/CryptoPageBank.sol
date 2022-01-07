01d1009895681a1a053858ee831be2e3c75540cf
vladimirmyshkovski <vladimirmyshkovski@gmail.com>
2022-01-07 21:28:32 +0400

    Remove unused imports, add comments, change logic in burn

--- contracts/CryptoPageBank.sol
+++ contracts/CryptoPageBank.sol
@@ -3,27 +3,43 @@
 pragma solidity ^0.8.3;
 
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
 import "./interfaces/ICryptoPageBank.sol";
 import "./interfaces/ICryptoPageToken.sol";
 import "./interfaces/ICryptoPageCommentDeployer.sol";
 
+/// @title The contract calculates amount and mint / burn PAGE tokens
+/// @author Crypto.Page Team
+/// @notice
+/// @dev
 contract PageBank is OwnableUpgradeable, IPageBank {
     using SafeMathUpgradeable for uint256;
 
+    /// Address of Crypto.Page treasury
     address public treasury;
+    /// Address of CryptoPageNFT contract
     address public nft;
+    /// Address of CryptoPageCommentDeployer contract
     address public commentDeployer;
+    /// Treasury fee (1000 is 10%, 100 is 1% 10 is 0.1%)
     uint256 public treasuryFee;
+    /// CryptoPageToken interface
     IPageToken public token;
-    IUniswapV3Pool public usdtpagePool;
-    IUniswapV3Pool public wethusdtPool;
+    // UniswapV3Pool interface for USDT / PAGE pool
+    IUniswapV3Pool private usdtpagePool;
+    // UniswapV3Pool interface for WETH / USDT pool
+    IUniswapV3Pool private wethusdtPool;
 
+    address private weth;
+    address private usdt;
+
+    // Storage balance per address
     mapping(address => uint256) private _balances;
 
+    /// Modifier for functions that can call only from CryptoPageNFT address
     modifier onlyNFT() {
         require(
             _msgSender() == nft,
@@ -32,6 +48,7 @@ contract PageBank is OwnableUpgradeable, IPageBank {
         _;
     }
 
+    /// Modifier for functions that can call only from CryptoPageCommentDeployer address
     modifier onlyCommentDeployer() {
         require(
             _msgSender() == commentDeployer,
@@ -50,7 +67,8 @@ contract PageBank is OwnableUpgradeable, IPageBank {
         address _token,
         address _nft,
         address _commentDeployer,
-        uint256 _treasuryFee
+        uint256 _treasuryFee,
+        address _factory
     ) public initializer {
         __Ownable_init();
         treasury = _treasury;
@@ -58,6 +76,9 @@ contract PageBank is OwnableUpgradeable, IPageBank {
         nft = _nft;
         treasuryFee = _treasuryFee;
         commentDeployer = _commentDeployer;
+        IUniswapV3Factory factory = IUniswapV3Factory(_factory);
+        wethusdtPool = IUniswapV3Pool(factory.getPool(weth, usdt, 3000));
+        usdtpagePool = IUniswapV3Pool(factory.createPool(usdt, address(token), 3000));
     }
 
     /// @notice Function for calling from PageNFT.safeMint
@@ -68,10 +89,7 @@ contract PageBank is OwnableUpgradeable, IPageBank {
         payable
         override
         onlyNFT
-        returns (
-            // onlyRole(MINTER_ROLE)
-            uint256
-        )
+        returns (uint256)
     {
         uint256 amount = _calculateAmount(gas);
         uint256 treasuryAmount = _calculateTreasuryAmount(amount);
@@ -101,15 +119,18 @@ contract PageBank is OwnableUpgradeable, IPageBank {
 
     /// @notice Function for calling from PageNFT.safeBurn
     /// @param to Owner's address of PAGE.NFT token
-    /// @param gas Amount of gas spent on the execution PageNFT.safeBurn function
+    /// @param commentsReward Amount of gas spent on the execution PageNFT.safeBurn function
     function burn(
         address to,
         uint256 gas,
-        uint256 burnPrice
+        uint256 commentsReward
     ) public payable override onlyNFT {
         uint256 amount = _calculateAmount(gas);
-        burnPrice = _calculateAmount(burnPrice);
-        amount = burnPrice.add(amount);
+        commentsReward = _calculateAmount(commentsReward);
+        if (commentsReward > amount) {
+            amount = commentsReward.sub(amount);
+        }
+
         require(token.balanceOf(_msgSender()) >= amount, "not enought balance");
         token.burn(to, amount);
     }
@@ -193,7 +214,6 @@ contract PageBank is OwnableUpgradeable, IPageBank {
     /// @param _amount Amount that needs to add
     function _addBalance(address _to, uint256 _amount)
         private
-        
         returns (uint256)
     {
         _balances[_to] += _amount;
@@ -205,7 +225,6 @@ contract PageBank is OwnableUpgradeable, IPageBank {
     /// @param _amount Amount that needs to substraction
     function _subBalance(address _to, uint256 _amount)
         private
-        
         returns (uint256)
     {
         _balances[_to] += _amount;
@@ -215,18 +234,14 @@ contract PageBank is OwnableUpgradeable, IPageBank {
     /// @notice Set amount to _balances
     /// @param _to Address to which to set
     /// @param _amount Amount that needs to set
-    function _setBalance(address _to, uint256 _amount) private  {
+    function _setBalance(address _to, uint256 _amount) private {
         _balances[_to] = _amount;
     }
 
     /// @notice Returns gas multiplied by token's prices and gas price.
     /// @param gas Comment author's address
     /// @return PAGE token's count
-    function _calculateAmount(uint256 gas)
-        private
-        view
-        returns (uint256)
-    {
+    function _calculateAmount(uint256 gas) private view returns (uint256) {
         return
             gas.mul(tx.gasprice).mul(getWETHUSDTPrice()).mul(
                 getUSDTPAGEPrice()
