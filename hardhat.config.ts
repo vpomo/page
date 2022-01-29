9be15b6838a343b016339c263e990903324fa40e
vladimirmyshkovski <vladimirmyshkovski@gmail.com>
2022-01-29 20:26:28 +0400

    Change solidity version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -64,7 +64,7 @@ const config: HardhatUserConfig = {
     solidity: {
         compilers: [
             {
-                version: "0.8.3",
+                version: "0.8.0",
                 settings: {
                     optimizer: {
                         enabled: true,
