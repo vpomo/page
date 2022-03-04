49027e2080714ce1769df5230c8cc09880690d25
vpomo <pomogalov@gmail.com>
2022-03-04 17:56:03 +0300

    added PageCommunity

---
+++ contracts/CryptoCommunity.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.11;
+
+import "@openzeppelin/contracts/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/access/AccessControlUpgradeable.sol";
+
+
+/// @title The contract for manage community
+/// @author Crypto.Page Team
+/// @notice
+/// @dev 
+contract PageCommunity is
+    Initializable,
+    OwnableUpgradeable,
+    AccessControlUpgradeable
+{
+
+}
