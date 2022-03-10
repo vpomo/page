357f139fac1ab4b7454a21295ac487655b26a813
vpomo <pomogalov@gmail.com>
2022-03-10 19:55:10 +0300

    added test fo test_page_community.py

--- tests/test_page_community.py
+++ tests/test_page_community.py
@@ -13,8 +13,44 @@ def test_deployment(pageCommunity):
 
 
 def test_add_community(pageCommunity):
-    pageCommunity.addCommunity("First users")
+    communityName = 'First users'
+    pageCommunity.addCommunity(communityName)
     assert pageCommunity.communityCount() == 1
     community = pageCommunity.getCommunity(1)
-    print('community', community)
+    assert community[0] == communityName
+
+
+def test_add_remove_moderator(pageCommunity, accounts):
+    communityName = 'First users'
+    pageCommunity.addCommunity(communityName)
+    assert pageCommunity.communityCount() == 1
+
+    pageCommunity.addModerator(1, accounts[2], {'from': accounts[0]})
+    pageCommunity.addModerator(1, accounts[3], {'from': accounts[0]})
+    community = pageCommunity.getCommunity(1)
+    assert community[2][0] == accounts[2]
+    assert community[2][1] == accounts[3]
+
+    pageCommunity.removeModerator(1, accounts[2], {'from': accounts[0]})
+    community = pageCommunity.getCommunity(1)
+    assert community[2][0] == accounts[3]
+
+
+def test_find_moderator(pageCommunity, accounts):
+    communityName = 'First users'
+    pageCommunity.addCommunity(communityName)
+    assert pageCommunity.communityCount() == 1
+
+    pageCommunity.addModerator(1, accounts[2], {'from': accounts[0]})
+    pageCommunity.addModerator(1, accounts[3], {'from': accounts[0]})
+    community = pageCommunity.getCommunity(1)
+    assert community[2][0] == accounts[2]
+    assert community[2][1] == accounts[3]
+
+    moderator = pageCommunity.findModerator(1, accounts[2], {'from': accounts[0]})
+    assert moderator == 0
+    moderator = pageCommunity.findModerator(1, accounts[3], {'from': accounts[0]})
+    assert moderator == 1
+
+
 
