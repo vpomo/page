8addbec4ad5919534cfa319e3189070042bff65a
vpomo <pomogalov@gmail.com>
2022-04-20 14:55:54 +0300

    fixed IPageVoteForEarn

--- contracts/interfaces/ICryptoPageVoteForEarn.sol
+++ contracts/interfaces/ICryptoPageVoteForEarn.sol
@@ -7,30 +7,29 @@ interface IPageVoteForEarn {
 
     function version() external pure returns (string memory);
 
-    function createVote(
+    function createPrivacyAccessPriceVote (
         uint256 communityId,
         string memory description,
         uint128 duration,
-        address user
+        uint256 newPrice
     ) external;
 
     function setMinDuration(uint128 minDuration) external;
 
-    function putVote(uint256 communityId, uint256 index, bool isYes) external;
+    function putPrivacyAccessPriceVote(uint256 communityId, uint256 index, bool isYes) external;
 
-    function executeVote(uint256 communityId, uint256 index) external;
+    function executePrivacyAccessPriceVote(uint256 communityId, uint256 index) external;
 
-    function readVote(uint256 index) external view returns(
+    function readPrivacyAccessPriceVote(uint256 communityId, uint256 index) external view returns(
         string memory description,
         address creator,
         uint128 finishTime,
         uint128 yesCount,
         uint128 noCount,
-        address user,
+        uint128 newPrice,
         address[] memory voteUsers,
-        uint256[] memory voteCommunities,
         bool active
     );
 
-    function readVotesCount() external view returns(uint256 count);
+    function readPrivacyAccessPriceVotesCount(uint256 communityId) public view returns(uint256 count);
 }
