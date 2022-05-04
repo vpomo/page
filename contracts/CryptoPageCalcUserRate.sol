f57b062bb945f8df01e950bb21bfa0b91e718a74
vpomo <pomogalov@gmail.com>
2022-05-04 21:00:42 +0300

    added logic for checkActivity

--- contracts/CryptoPageCalcUserRate.sol
+++ contracts/CryptoPageCalcUserRate.sol
@@ -12,19 +12,20 @@ import "./interfaces/ICryptoPageCommunity.sol";
 /// @title The contract calculates rates of users
 /// @author Crypto.Page Team
 /// @notice
-/// @dev 
+/// @dev
 contract PageCalcUserRate is
-    Initializable,
-    OwnableUpgradeable,
-    AccessControlUpgradeable,
-    IPageCalcUserRate
+Initializable,
+OwnableUpgradeable,
+AccessControlUpgradeable,
+IPageCalcUserRate
 {
 
     IPageUserRateToken public userRateToken;
     IPageCommunity public community;
 
     uint256 public constant TOKEN_ID_MULTIPLYING_FACTOR = 100;
-    bytes public constant FOR_RATE_TOKEN_DATA = "";
+    bytes public FOR_RATE_TOKEN_DATA = bytes(0);
+    uint256[10] public interestAdjustment = [10, 20, 30, 40, 50, 60, 20, 40, 20, 40];
 
     enum UserRatesType {
         RESERVE, HUNDRED_UP, THOUSAND_UP, HUNDRED_DOWN, THOUSAND_DOWN,
@@ -50,18 +51,18 @@ contract PageCalcUserRate is
     }
 
     mapping(uint256 => mapping(address => RateCount)) activityCounter;
-    mapping(uint256 => mapping(address => RateCount)) redeemedCounter;
+    mapping(uint256 => mapping(address => RedeemedCount)) redeemedCounter;
 
     /**
      * @dev Makes the initialization of the initial values for the smart contract
      *
      * @param _admin Address of admin
      * @param _community Address of community
-     * @param _bank Address of bank
+     * @param _userRateToken Address of bank
      */
     function initialize(address _admin, address _community, address _userRateToken)
-        public
-        initializer
+    public
+    initializer
     {
         __Ownable_init();
 
@@ -93,14 +94,110 @@ contract PageCalcUserRate is
         //revert("PageBank: asset transfer prohibited");
     }
 
+    function checkActivity(uint256 communityId, address user, ActivityType activityType) public
+    returns(int256 resultPercent)
+    {
+        addActivity(communityId, user, activityType);
+        uint256 baseTokenId = communityId * TOKEN_ID_MULTIPLYING_FACTOR;
+
+        checkMessages(baseTokenId, communityId, user);
+        checkPosts(baseTokenId, communityId, user);
+        checkUps(baseTokenId, communityId, user);
+        checkDowns(baseTokenId, communityId, user);
+
+        return calcPercent(communityId, user, baseTokenId);
+    }
+
+    function checkMessages(uint256 tokenId, uint256 communityId, address user) private {
+        uint256 realMessageCount = activityCounter[communityId][user].messageCount;
+
+        checkMessagesByIndex(tokenId, communityId, user, realMessageCount, 0);
+        checkMessagesByIndex(tokenId, communityId, user, realMessageCount, 1);
+        checkMessagesByIndex(tokenId, communityId, user, realMessageCount, 2);
+    }
+
+    function checkPosts(uint256 tokenId, uint256 communityId, address user) private {
+        uint256 realPostCount = activityCounter[communityId][user].postCount;
+
+        checkPostsByIndex(tokenId, communityId, user, realPostCount, 0);
+        checkPostsByIndex(tokenId, communityId, user, realPostCount, 1);
+        checkPostsByIndex(tokenId, communityId, user, realPostCount, 2);
+    }
+
+    function checkUps(uint256 tokenId, uint256 communityId, address user) private {
+        uint256 realUpCount = activityCounter[communityId][user].upCount;
+
+        checkUpsByIndex(tokenId, communityId, user, realUpCount, 0);
+        checkUpsByIndex(tokenId, communityId, user, realUpCount, 1);
+    }
+
+    function checkDowns(uint256 tokenId, uint256 communityId, address user) private {
+        uint256 realDownCount = activityCounter[communityId][user].downCount;
+
+        checkDownsByIndex(tokenId, communityId, user, realDownCount, 0);
+        checkDownsByIndex(tokenId, communityId, user, realDownCount, 1);
+    }
+
+    function checkMessagesByIndex(uint256 tokenId, uint256 communityId, address user, uint256 realMessageCount, uint256 index) private {
+        RedeemedCount storage redeemCounter = redeemedCounter[communityId][user];
+
+        uint256 number = realMessageCount / (10 * 10**index);
+        uint64 mintNumber = uint64(number) - redeemCounter.messageCount[index];
+        if (mintNumber > 0) {
+            redeemCounter.messageCount[index] += mintNumber;
+            userRateToken.mint(
+                user, tokenId + uint256(UserRatesType.TEN_MESSAGE + index), mintNumber, FOR_RATE_TOKEN_DATA
+            );
+        }
+    }
+
+    function checkPostsByIndex(uint256 tokenId, uint256 communityId, address user, uint256 realPostCount, uint256 index) private {
+        RateCount storage redeemCounter = redeemedCounter[communityId][user];
+
+        uint256 number = realPostCount / (10 * 10**index);
+        uint256 mintNumber = number - redeemCounter.postCount[index];
+        if (mintNumber > 0) {
+            redeemCounter.postCount[index] += mintNumber;
+            userRateToken.mint(
+                user, tokenId + uint256(UserRatesType.TEN_POST + index), mintNumber, FOR_RATE_TOKEN_DATA
+            );
+        }
+    }
+
+    function checkUpsByIndex(uint256 tokenId, uint256 communityId, address user, uint256 realUpCount, uint256 index) private {
+        RateCount storage redeemCounter = redeemedCounter[communityId][user];
+
+        uint256 number = realUpCount / (10 * 10**(index+1));
+        uint256 mintNumber = number - redeemCounter.upCount[index];
+        if (mintNumber > 0) {
+            redeemCounter.upCount[index] += mintNumber;
+            userRateToken.mint(
+                user, tokenId + uint256(UserRatesType.HUNDRED_UP + index), mintNumber, FOR_RATE_TOKEN_DATA
+            );
+        }
+    }
+
+    function checkDownsByIndex(uint256 tokenId, uint256 communityId, address user, uint256 realDownCount, uint256 index) private {
+        RateCount storage redeemCounter = redeemedCounter[communityId][user];
+
+        uint256 number = realDownCount / (10 * 10**(index+1));
+        uint256 mintNumber = number - redeemCounter.downCount[index];
+        if (mintNumber > 0) {
+            redeemCounter.downCount[index] += mintNumber;
+            userRateToken.mint(
+                user, tokenId + uint256(UserRatesType.HUNDRED_DOWN + index), mintNumber, FOR_RATE_TOKEN_DATA
+            );
+        }
+    }
+
     /**
      * @dev .
      *
      * @param communityId ID of community
-     * @param user
-     * @param activityType
+     * @param user aaa
+     * @param activityType aa
      */
-    function addActivity(uint256 communityId, address user, ActivityType activityType) external override {
+    function addActivity(uint256 communityId, address user, ActivityType activityType) private {
         RateCount storage counter = activityCounter[communityId][user];
         if (activityType == ActivityType.POST) {
             counter.postCount++;
@@ -116,94 +213,31 @@ contract PageCalcUserRate is
         }
     }
 
-    function checkActivity(uint256 communityId, address user) public {
-        uint256 tokenId = communityId * TOKEN_ID_MULTIPLYING_FACTOR;
+    function calcPercent(uint256 communityId, address user, uint256 baseTokenId) private returns(int256 resultPercent) {
+        resultPercent = 0;
+        uint256[10] memory weight = interestAdjustment;
+        uint256[] memory messageAmount = new uint256[](3);
+        uint256[] memory postAmount = new uint256[](3);
+        uint256[] memory upAmount = new uint256[](2);
+        uint256[] memory downAmount = new uint256[](2);
 
-        checkMessages(tokenId, communityId, user);
-        checkPosts(tokenId, communityId, user);
-        checkUps(tokenId, communityId, user);
-        checkDowns(tokenId, communityId, user);
-    }
+        messageAmount[0] = userRateToken.balanceOf(user, baseTokenId + uint256(UserRatesType.TEN_MESSAGE + 0));
+        messageAmount[1] = userRateToken.balanceOf(user, baseTokenId + uint256(UserRatesType.TEN_MESSAGE + 1));
+        messageAmount[2] = userRateToken.balanceOf(user, baseTokenId + uint256(UserRatesType.TEN_MESSAGE + 2));
 
-    function checkMessages(uint256 communityId, address user) private {
-        uint256 realMessageCount = activityCounter[communityId][user].messageCount;
+        postAmount[0] = userRateToken.balanceOf(user, baseTokenId + uint256(UserRatesType.TEN_POST + 0));
+        postAmount[1] = userRateToken.balanceOf(user, baseTokenId + uint256(UserRatesType.TEN_POST + 1));
+        postAmount[2] = userRateToken.balanceOf(user, baseTokenId + uint256(UserRatesType.TEN_POST + 2));
 
-        checkMessagesByIndex(communityId, user, realMessageCount, 0);
-        checkMessagesByIndex(communityId, user, realMessageCount, 1);
-        checkMessagesByIndex(communityId, user, realMessageCount, 2);
-    }
+        upAmount[0] = userRateToken.balanceOf(user, baseTokenId + uint256(UserRatesType.HUNDRED_UP + 0));
+        upAmount[1] = userRateToken.balanceOf(user, baseTokenId + uint256(UserRatesType.HUNDRED_UP + 1));
 
-    function checkPosts(uint256 tokenId, uint256 communityId, address user) private {
-        uint256 realPostCount = activityCounter[communityId][user].postCount;
+        downAmount[0] = userRateToken.balanceOf(user, baseTokenId + uint256(UserRatesType.HUNDRED_DOWN + 0));
+        downAmount[1] = userRateToken.balanceOf(user, baseTokenId + uint256(UserRatesType.HUNDRED_DOWN + 1));
 
-        checkPostsByIndex(tokenId, user, realPostCount, 0);
-        checkPostsByIndex(tokenId, user, realPostCount, 1);
-        checkPostsByIndex(tokenId, user, realPostCount, 2);
-    }
-
-    function checkUps(uint256 tokenId, uint256 communityId, address user) private {
-        uint256 realUpCount = activityCounter[communityId][user].upCount;
-
-        checkUpsByIndex(tokenId, user, realUpCount, 0);
-        checkUpsByIndex(tokenId, user, realUpCount, 1);
-    }
-
-    function checkDowns(uint256 tokenId, uint256 communityId, address user) private {
-        uint256 realDownCount = activityCounter[communityId][user].downCount;
-
-        checkDownsByIndex(tokenId, user, realDownCount, 0);
-        checkDownsByIndex(tokenId, user, realDownCount, 1);
-    }
-
-    function checkMessagesByIndex(uint256 tokenId, address user, uint256 realMessageCount, uint256 index) private {
-        RateCount storage redeemCounter = redeemedCounter[communityId][user];
-
-        uint256 number = realMessageCount / (10 * 10**index);
-        uint256 mintNumber = number - redeemCounter.messageCount[index];
-        if (mintNumber > 0) {
-            redeemCounter.messageCount[index] += mintNumber;
-            userRateToken.mint(
-                user, tokenId + uint256(UserRatesType.TEN_MESSAGE + index), mintNumber, FOR_RATE_TOKEN_DATA
-            );
-        }
-    }
-
-    function checkPostsByIndex(uint256 tokenId, address user, uint256 realPostCount, uint256 index) private {
-        RateCount storage redeemCounter = redeemedCounter[communityId][user];
-
-        uint256 number = realPostCount / (10 * 10**index);
-        uint256 mintNumber = number - redeemCounter.postCount[index];
-        if (mintNumber > 0) {
-            redeemCounter.postCount[index] += mintNumber;
-            userRateToken.mint(
-                user, tokenId + uint256(UserRatesType.TEN_POST + index), mintNumber, FOR_RATE_TOKEN_DATA
-            );
-        }
-    }
-
-    function checkUpsByIndex(uint256 tokenId, address user, uint256 realUpCount, uint256 index) private {
-        RateCount storage redeemCounter = redeemedCounter[communityId][user];
-
-        uint256 number = realUpCount / (10 * 10**(index+1));
-        uint256 mintNumber = number - redeemCounter.upCount[index];
-        if (mintNumber > 0) {
-            redeemCounter.upCount[index] += mintNumber;
-            userRateToken.mint(
-                user, tokenId + uint256(UserRatesType.HUNDRED_UP + index), mintNumber, FOR_RATE_TOKEN_DATA
-            );
-        }
-    }
-
-    function checkDownsByIndex(uint256 tokenId, address user, uint256 realDownCount, uint256 index) private {
-        RateCount storage redeemCounter = redeemedCounter[communityId][user];
-
-        uint256 number = realDownCount / (10 * 10**(index+1));
-        uint256 mintNumber = number - redeemCounter.downCount[index];
-        if (mintNumber > 0) {
-            redeemCounter.downCount[index] += mintNumber;
-            userRateToken.mint(
-                user, tokenId + uint256(UserRatesType.HUNDRED_DOWN + index), mintNumber, FOR_RATE_TOKEN_DATA
-            );
-        }
+        resultPercent += weight[0] * messageAmount[0] + weight[1] * messageAmount[1] + weight[2] * messageAmount[2];
+        resultPercent += weight[3] * postAmount[0] + weight[4] * postAmount[1] + weight[5] * postAmount[2];
+        resultPercent += weight[6] * upAmount[0] + weight[7] * upAmount[1];
+        resultPercent -= weight[8] * downAmount[0] + weight[9] * downAmount[1];
     }
 }
