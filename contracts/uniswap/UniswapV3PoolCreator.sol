b7f080128284ba4bb4e594f2d235009f2c2ff049
vpomo <pomogalov@gmail.com>
2022-05-11 12:29:10 +0300

    fixed poll creator

--- contracts/uniswap/UniswapV3PoolCreator.sol
+++ contracts/uniswap/UniswapV3PoolCreator.sol
@@ -9,7 +9,7 @@ import "@uniswap/contracts/interfaces/IUniswapV3Pool.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "../interfaces/INonfungiblePositionManager.sol";
 
-import "@uniswap/contracts/libraries/TransferHelper.sol";
+import "@uniswap-periphery/contracts/libraries/TransferHelper.sol";
 
 contract UniswapV3PoolCreator is IERC721Receiver {
     /// @notice Represents the deposit of an NFT
