e958c6e7e7edc9b15d747fe3d98c2828db875787
vpomo <pomogalov@gmail.com>
2022-05-16 18:43:13 +0300

    added logic for SafeDeal

--- contracts/CryptoPageSafeDeal.sol
+++ contracts/CryptoPageSafeDeal.sol
@@ -20,9 +20,13 @@ contract CryptoPageSafeDeal is
 
     uint256 public dealCount;
 
+    struct DealMessage {
+        string message;
+        uint256 writeTime;
+    }
+
     struct SafeDeal {
         string description;
-        string[] messages;
         address sideA;
         address sideB;
         address guarantor;
@@ -35,9 +39,14 @@ contract CryptoPageSafeDeal is
         bool endApproveB;
         bool isIssue;
         bool isFinished;
+        DealMessage[] messages;
     }
 
     mapping(uint256 => SafeDeal) private deals;
+
+    event MakeDeal(address indexed creator, uint256 number);
+    event ChangeDescription(uint256 dealId, string description);
+    event ChangeTime(uint256 dealId, uint128 startTime, uint128 endTime);
 
     /**
      * @dev Makes the initialization of the initial values for the smart contract
@@ -49,13 +58,60 @@ contract CryptoPageSafeDeal is
         public
         initializer
     {
-        require(_admin != address(0), "PageBank: wrong address");
-        require(_calcUserRate != address(0), "PageBank: wrong address");
+        require(_admin != address(0), "SafeDeal: wrong address");
+        require(_calcUserRate != address(0), "SafeDeal: wrong address");
 
         _setupRole(DEFAULT_ADMIN_ROLE, _admin);
         calcUserRate = IPageCalcUserRate(_calcUserRate);
     }
 
+    function makeDeal(
+        string memory desc,
+        address sideA,
+        address sideB,
+        uint128 startTime,
+        uint128 endTime,
+        uint256 amount
+    ) external override {
+        require(block.timestamp < startTime && startTime < endTime, "SafeDeal: wrong time");
+
+        dealCount++;
+        SafeDeal storage deal = deals[dealCount];
+        deal.description = desc;
+        deal.sideA = sideA;
+        deal.sideB = sideB;
+        deal.startTime = startTime;
+        deal.endTime = endTime;
+        deal.amount = amount;
+
+        emit MakeDeal(_msgSender(), dealCount);
+    }
+
+    function changeDescription(uint256 dealId, string memory desc) external override {
+        address sender = _msgSender();
+        SafeDeal storage deal = deals[dealCount];
+        require(sender == deal.sideA || sender == deal.sideB, "SafeDeal: wrong sender");
+        deal.description = desc;
+
+        emit ChangeDescription(dealId, desc);
+    }
+
+    function changeTime(uint256 dealId, uint128 startTime, uint128 endTime) external override {
+        address sender = _msgSender();
+        SafeDeal storage deal = deals[dealCount];
+        require(sender == deal.sideA || sender == deal.sideB, "SafeDeal: wrong sender");
+        if(startTime > 0) {
+            require(block.timestamp < startTime, "SafeDeal: wrong start time");
+            deal.startTime = startTime;
+        }
+
+        if(endTime > 0) {
+            require(block.timestamp < deal.startTime && deal.startTime < endTime, "SafeDeal: wrong end time");
+            deal.endTime = endTime;
+        }
+
+        emit ChangeTime(dealId, startTime, endTime);
+    }
 
 
 }
