95ecb90dc05f050167d549c11a913be6ba62d6ec
vpomo <pomogalov@gmail.com>
2022-06-10 16:19:51 +0300

    Added control via proxy contract

--- scripts/deploy_page_bank.py
+++ scripts/deploy_page_bank.py
@@ -5,8 +5,6 @@ from deploy import config
 def main():
     deployer = config.get_deployer_account(config.get_is_live())
     admin = config.get_admin()
-    treasury = deployer
-    calcUserRate = PageCalcUserRate.at("0x7e47dd1b1689b9ab3ce3c54e2ccb9a97054c52ac")
 
     print("Deployer:", deployer)
 
